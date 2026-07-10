b699658e31ce3c914b3f3e0ccec06f9b8d034468
quilonamjad-spec <quilonamjad@gmail.com>
2026-07-10 19:26:39 +0530

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -3,6 +3,14 @@ import streamlit as st
 from ui.styles import load_css
 from ui.components.stock_card import StockCard
 
+# Backend
+from data.market_data import MarketDataEngine
+from indicators.ema import EMAEngine
+
+# ---------------------------------------------------
+# Page
+# ---------------------------------------------------
+
 st.set_page_config(
     page_title="TDE Prototype",
     layout="centered",
@@ -10,15 +18,50 @@ st.set_page_config(
 
 load_css()
 
+# ---------------------------------------------------
+# Download Market Data
+# ---------------------------------------------------
+
+market = MarketDataEngine()
+
+data = market.download_data(["SBIN.NS"])
+
+df = data["SBIN.NS"]
+
+# ---------------------------------------------------
+# Calculate EMA20
+# ---------------------------------------------------
+
+ema = EMAEngine()
+
+df = ema.calculate(df)
+
+# ---------------------------------------------------
+# Latest Values
+# ---------------------------------------------------
+
+price = df["Close"].iloc[-1]
+
+previous = df["Close"].iloc[-2]
+
+change = price - previous
+
+change_percent = (change / previous) * 100
+
+# ---------------------------------------------------
+# Render Card
+# ---------------------------------------------------
+
 card = StockCard()
 
 card.render(
     ticker="SBIN",
     company="State Bank of India",
-    price=1022.60,
-    change=-4.60,
-    change_percent=-0.45,
+    price=price,
+    change=change,
+    change_percent=change_percent,
     trend="Bearish",
     momentum="Weakening",
     risk="Recovering",
+    df=df,                # <-- New parameter
 )
