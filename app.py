7869bc84775b3f81bc6dada2c6bcbf5324459dce
quilonamjad-spec <quilonamjad@gmail.com>
2026-07-10 08:51:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,90 +1,38 @@
 import streamlit as st
 
-from ui.cards import stock_card
-from ui.styles import load_css
-
-from utils.watchlist import load_watchlist
-from data.fetch_data import fetch_stock_data
-
+from data.market_data import MarketDataEngine
+from engine.indicator_engine import IndicatorEngine
+from ui.dashboard import Dashboard
 
 st.set_page_config(
     page_title="Trade Decision Engine",
-    page_icon="📈",
     layout="wide"
 )
 
-load_css()
-
 st.title("📈 Trade Decision Engine")
+st.caption("Version 0.4")
 
-st.caption("Professional Multi Stock Scanner")
+# -------------------------------------
 
-st.divider()
+market = MarketDataEngine().download_watchlist()
 
-watchlist = load_watchlist()
+analysis = IndicatorEngine().analyse_market(market)
 
-stocks=[]
+dashboard = Dashboard()
 
-for i,ticker in enumerate(watchlist):
+# -------------------------------------
 
-    live = fetch_stock_data(ticker)
+cols = st.columns(3)
 
-    stock = {
+i = 0
 
-        "rank":i+1,
+for ticker, result in analysis.items():
 
-        "ticker":live["ticker"],
+    with cols[i % 3]:
 
-        "company":live["company"],
+        dashboard.show_stock_card(
+            ticker,
+            result
+        )
 
-        "price":live["price"],
-
-        "change":f'{live["change"]:+.2f}',
-
-        # Dummy Values (Next Version)
-
-        "recommendation":"WAIT",
-
-        "score":50,
-
-        "trend":"Neutral",
-
-        "momentum":"Neutral",
-
-        "volume":"Average",
-
-        "ema":"Neutral",
-
-        "macd":"Neutral"
-
-    }
-
-    stocks.append(stock)
-
-col1,col2,col3,col4 = st.columns(4)
-
-with col1:
-
-    st.metric("Stocks",len(stocks))
-
-with col2:
-
-    st.metric("Data Source","Yahoo")
-
-with col3:
-
-    st.metric("Status","LIVE")
-
-with col4:
-
-    st.metric("Version","0.4")
-
-st.divider()
-
-cols=st.columns(3)
-
-for i,stock in enumerate(stocks):
-
-    with cols[i%3]:
-
-        stock_card(stock)
+    i += 1
