e7718c3b8cb84446124b81361550d8b7d2df648c
quilonamjad-spec <quilonamjad@gmail.com>
2026-07-11 13:31:12 +0530

    Update test_EMA_1.py

--- tests/test_EMA_1.py
+++ tests/test_EMA_1.py
@@ -1,4 +1,19 @@
 from indicators import ema
+from data.market_data import MarketData
+
+# -----------------------------------
+# Download Data
+# -----------------------------------
+
+market = MarketData()
+
+data = market.download_data(["SBIN.NS"])
+
+df = data["SBIN.NS"]
+
+# -----------------------------------
+# Run EMA Engine
+# -----------------------------------
 
 result = ema.calculate(df)
 
