63959c295a2c90d8a2a4c6110d68136aab1fbe56
quilonamjad-spec <quilonamjad@gmail.com>
2026-07-11 08:21:42 +0530

    Update test_trade_quality.py

--- tests/test_trade_quality.py
+++ tests/test_trade_quality.py
@@ -1,1 +1,18 @@
+from engine.trade_quality import TradeQualityEngine
 
+
+engine = TradeQualityEngine()
+
+result = engine.calculate(
+
+    trend_score=32,
+
+    momentum_score=28,
+
+    risk_score=24,
+
+    direction="LONG"
+
+)
+
+print(result)
