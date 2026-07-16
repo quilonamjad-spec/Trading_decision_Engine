69c45ba80db7b8c23e2631a8f24617fe66f3d802
quilonamjad-spec <quilonamjad@gmail.com>
2026-07-16 19:06:53 +0530

    Update consensus.py

--- engine/consensus.py
+++ engine/consensus.py
@@ -103,20 +103,20 @@ class ConsensusEngine:
         # Result
         # ---------------------------------------------
 
-        return {
+       return {
 
             "agreement": agreement,
-
+        
             "confidence_score": confidence_total,
-
+        
+            "consensus": round(multiplier * 100),
+        
             "multiplier": multiplier,
-
+        
             "reason":
-
+        
                 f"{agreement} consensus | "
-
-                f"Confidence "
-
-                f"{confidence_total}/9"
-
+        
+                f"Confidence {confidence_total}/9"
+        
         }
