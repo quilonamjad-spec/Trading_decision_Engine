8651da2aa38b4e14889683c683b4dbba127ba65e
quilonamjad-spec <quilonamjad@gmail.com>
2026-07-20 06:57:07 +0530

    Update app_trading_desk.py

--- app_trading_desk.py
+++ app_trading_desk.py
@@ -1,9 +1,19 @@
 import streamlit as st
 
+from engine.trading_desk_engine import TradingDeskEngine
+from ui.trading_desk import draw_trading_desk
+
+
 st.set_page_config(
     page_title="Trading Desk",
     page_icon="📡",
     layout="wide"
 )
 
-st.title("📡 Trading Desk")
+st.title("Trading Desk")
+
+engine = TradingDeskEngine()
+
+data = engine.build()
+
+draw_trading_desk(data)
