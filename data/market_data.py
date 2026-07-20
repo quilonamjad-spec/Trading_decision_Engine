687e9e0b31d9cbbd2f741a6b276cfed6dfd65de8
quilonamjad-spec <quilonamjad@gmail.com>
2026-07-20 08:20:57 +0530

    Update market_data.py

--- data/market_data.py
+++ data/market_data.py
@@ -78,10 +78,10 @@ class MarketDataEngine:
 
         end_datetime = pd.Timestamp(end_datetime)
         
-        if end_datetime.tzinfo is None:
-            end_datetime = end_datetime.tz_localize("Asia/Kolkata")
-        else:
-            end_datetime = end_datetime.tz_convert("Asia/Kolkata")
+            if end_datetime.tzinfo is None:
+                end_datetime = end_datetime.tz_localize("Asia/Kolkata")
+            else:
+                end_datetime = end_datetime.tz_convert("Asia/Kolkata")
         
         df = df[df.index <= end_datetime]
     
